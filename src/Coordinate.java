86346f3af34d638c6353b6d765e149302d3b1498
Max Hansson <maxh88@gmail.com>
2019-02-12 15:00:37 +0100

    added Coordinate helper class

---
+++ src/Coordinate.java
@@ -0,0 +1,22 @@
+public class Coordinate {
+    private int xPos;
+    private int yPos;
+
+    /**
+     * Constructor.
+     * @param xPos The initial x position.
+     * @param yPos The initial y position.
+     */
+    public Coordinate(int xPos, int yPos) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    public int getXPos() {
+        return this.xPos;
+    }
+
+    public int getYPos() {
+        return this.yPos;
+    }
+}
