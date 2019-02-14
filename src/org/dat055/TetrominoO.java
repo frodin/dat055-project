8e759bb69ed2c1409a15794980ce3728f1586168
Philip Hellberg <hellberg.ph@gmail.com>
2019-02-14 15:45:42 +0100

    Skapat två klasser, för tetromino I och O

---
+++ src/org/dat055/TetrominoO.java
@@ -0,0 +1,16 @@
+package org.dat055;
+
+public class TetrominoO extends ActiveTetromino{
+
+    public TetrominoO (String color){
+
+        // Här skapas state 0 för tetromino O (behövs bara state 0 då Tetromino O är identisk i alla states)
+
+        states.add(new State(color,
+                new Coordinate(1,0),
+                new Coordinate(2,0),
+                new Coordinate(1,1),
+                new Coordinate(2,1)));
+    }
+
+}
