016ca25c56a9050888966a42d6d44fb0be0d7d40
Albin Becevic <albinbecevic@Albins-MacBook-Pro.local>
2019-02-14 14:45:29 +0100

    Included State instead of coordinates and cell

--- src/org/dat055/ActiveTetromino.java
+++ src/org/dat055/ActiveTetromino.java
@@ -9,30 +9,26 @@ import java.util.HashMap;
  * ska ha ett nuvarande state, privat variable med getter
  * ska ha en rotate()-funktion som ändrar nuvarande state till nästkommande state
  */
-public class ActiveTetronimo {
-    private ArrayList<HashMap<Coordinate, Cell>> states; //List of all our diffrent tetromino states (represented by hashmaps)
-    private HashMap<Coordinate, Cell> state;
-    private HashMap<Coordinate, Cell> currentState; //Which rotationen the current tetromino has
+public class ActiveTetromino {
+    private ArrayList<State> states; //List of all our diffrent tetromino states (represented by hashmaps)
+    private State currentState; //Which rotationen the current tetromino has
     private int stateIndex;
 
     /**
      * Initiates our instance variables
      */
-    public ActiveTetronimo(){
+    public ActiveTetromino(){
         states = new ArrayList<>();
-        state  = new HashMap<Coordinate, Cell>();
         currentState = states.get(0);
         stateIndex = 0;
     }
 
-    public HashMap<Coordinate,Cell> getState(){ //return current state
+    public State getState(){ //return current state
         return currentState;
     }
+
     public void rotate(){ //give us next state in the list
-        stateIndex = stateIndex % 4 + 1;
-        if(stateIndex == 4){ //Jump back to zero, prevent the currentState to get out of bounds
-            stateIndex = 0;
-        }
+        stateIndex = stateIndex++ % 4;
         currentState = states.get(stateIndex);
     }
 
