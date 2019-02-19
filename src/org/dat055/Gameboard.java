f40fdb9f37a511bba599083147d24b33ed564170
Albin Becevic <albinbecevic@Albins-MacBook-Pro.local>
2019-02-19 13:51:43 +0100

    added null insted of cell

--- src/org/dat055/Gameboard.java
+++ src/org/dat055/Gameboard.java
@@ -6,16 +6,16 @@ public class Gameboard {
     private HashMap<Coordinate, Cell> gameboard;
     private int y, x;
 
-
     public Gameboard(int xLength, int yLength){
         makeGameboard(xLength, yLength);
 
     }
+
     public void makeGameboard(int xLength, int yLength){
         for(int i = 0; i < xLength * yLength; i++){
             x = i % yLength;
             y = i / xLength;
-            gameboard.put(new Coordinate(x, y), new Cell());
+            gameboard.put(new Coordinate(x, y), null);
         }
     }
 }
