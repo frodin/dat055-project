8614a415b0b4524d9c809df0cc4fa3751f0149c4
Max Hansson <maxh88@gmail.com>
2019-02-20 11:07:26 +0100

    add button event handler programmatically

--- src/org/dat055/views/MenuView.java
+++ src/org/dat055/views/MenuView.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import org.dat055.Gameboard;
@@ -13,15 +14,26 @@ import java.io.IOException;
 
 public class MenuView {
     Gameboard gameBoard;
+    @FXML Button newGameButton;
 
     public MenuView(Gameboard gameBoard) {
         this.gameBoard = gameBoard;
     }
 
     @FXML
-    public void newGame(MouseEvent event) throws IOException {
+    public void initialize() {
+        newGameButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> newGame(event));
+    }
+
+    @FXML
+    public void newGame(MouseEvent event) {
         Stage rootStage = (Stage) ((Node) event.getSource()).getScene().getWindow(); // lol java
-        Parent game = FXMLLoader.load(getClass().getResource("game_view.fxml"));
+        Parent game = null;
+        try {
+            game = FXMLLoader.load(getClass().getResource("game_view.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         rootStage.setScene(new Scene(game));
     }
 
