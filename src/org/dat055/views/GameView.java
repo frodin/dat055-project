fb1e1c8a250ef3200e32fe1a6ca04ef9c491d983
Max Hansson <maxh88@gmail.com>
2019-02-20 14:44:36 +0100

    create helper function for game field

--- src/org/dat055/views/GameView.java
+++ src/org/dat055/views/GameView.java
@@ -1,37 +1,30 @@
 package org.dat055.views;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
-import org.dat055.GameBoardInterface;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import org.dat055.Gameboard;
 
 public class GameView {
-    @FXML private GridPane board;
+    @FXML private GridPane field;
     private Gameboard gameBoard;
 
     public GameView(Gameboard gameBoard) {
         this.gameBoard = gameBoard;
     }
-    
+
     @FXML
     private void initialize() {
-        board.setHgap(0);
-        board.setVgap(0);
+        field = createGameField(this.gameBoard);
+    }
 
-        board.add(new Button(), 0, 0);
-        board.add(new Button(), 1, 1);
-        board.add(new Button(), 2, 2);
-
-        // add columns to grid
-        ColumnConstraints columnConstraints = new ColumnConstraints();
-        columnConstraints.setFillWidth(true);
-        columnConstraints.setHgrow(Priority.ALWAYS);
-        // todo: det ska nog finnas gameBoard.y RowConstraints och gameBoard.x ColumnConstraints
-        board.getColumnConstraints().add(columnConstraints);
-
-
+    private static GridPane createGameField(Gameboard gameBoard) {
+        GridPane grid = new GridPane();
+        grid.setHgap(0);
+        grid.setVgap(0);
+        return grid;
     }
 }
