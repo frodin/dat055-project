f7f63a18689b1092f2afe2fc9d13318c8a232285
Max Hansson <maxh88@gmail.com>
2019-02-21 13:52:58 +0100

    start writing some utility functions

--- src/org/dat055/GameboardController.java
+++ src/org/dat055/GameboardController.java
@@ -1,4 +1,18 @@
 package org.dat055;
 
 public class GameboardController {
+    private Gameboard gameBoard;
+
+    public Coordinate getTetrominoPosition() {
+        return gameBoard.getTetrominoPosition();
+    }
+    public void setTetrominoPosition(int x, int y) {
+        gameBoard.setTetrominoPosition(new Coordinate(x, y));
+    }
+    public void tick() {
+
+    }
+    public void createRandomTetromino() {
+        gameBoard.createTetromino();
+    }
 }
