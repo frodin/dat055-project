d19fe469109003e735bae56b0b84b913e592ac87
frodin <frodin@student.chalmers.se>
2019-03-07 15:56:50 +0100

    water cleansing

--- Tetris/src/org/dat055/GameBoardInterface.java
+++ Tetris/src/org/dat055/GameBoardInterface.java
@@ -1,29 +1,43 @@
 package org.dat055;
 
-
 import java.util.HashMap;
 
 public interface GameBoardInterface {
 
     Cell getCell(Coordinate coord);
+
     void setCell(Coordinate coord, Cell cell);
+
     HashMap<Coordinate, Cell> getGameBoard();
+
     Cell getCell(int xPos, int yPos);
+
     void setCell(int xPos, int yPos, Cell cell);
+
     void removeCell(int xPos, int yPos);
 
     Coordinate getTetrominoPosition();
+
     void setTetrominoPosition(Coordinate coord);
-    HashMap<Coordinate,Cell> getTetrominoCells();
+
+    HashMap<Coordinate, Cell> getTetrominoCells();
+
     void rotateTetromino();
+
     void killTetromino();
+
     void createTetromino();
+
     void createTetromino(int i);
+
     HashMap<Coordinate, Cell> getNextTetrominoCells();
 
     int getHeight();
+
     int getWidth();
+
     int getLevel();
+
     void levelUp();
 
 }
